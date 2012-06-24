3502feb58b78c7a76f8333255a91bd206a20a51b
T S <yenthefirst@gmail.com>
2012-06-23 22:45:13 -0700

    create inventory log model

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
-from elixir import metadata, Entity, Field, Integer, UnicodeText, using_options, BLOB, Enum
+from elixir import metadata, Entity, Field, using_options
+from elixir import Integer, UnicodeText, BLOB, Enum, DateTime
+from elixir import ManyToOne, OneToMany
 
 metadata.bind = "sqlite:///inventory.sqlite3"
 
@@ -9,6 +11,7 @@ class InvCard(Entity):
 	scan_png = Field(BLOB)
 	box_index = Field(Integer)
 	recognition_status = Field(Enum('scanned','candidate_match','incorrect_match','verified'))
+	inv_logs = OneToMany('InvLog')
 
 	rowid = Field(Integer, primary_key=True)
 
@@ -18,6 +21,23 @@ class InvCard(Entity):
 	def __repr__(self):
 		return "<%s/%s (%s/%d)>" % (self.set_name, self.name, self.box, self.box_index)
 
-#class InvLog(Entity):
-#	card_rowid
+class InvLog(Entity):
+	card = ManyToOne('InvCard')
+	direction = Field(Enum('added', 'removed'))
+	reason = Field(UnicodeText)
+	date = Field(DateTime)
+	#I wish I had a ActiveRecord-like 'timestamps' here
+
+	rowid = Field(Integer, primary_key = True)
+
+	using_options(tablename='inv_logs')
+
+	def __repr__(self):
+		if self.direction == u'added':
+			dir_text = 'added to'
+		else:
+			dir_text = 'removed_from'
+		card_repr = "%s/%s(%d)" % (self.card.set_name, self.card.name, self.card.rowid)
+
+		return "<%s: %s %s %s. \"%s\">" % (self.date, card_repr, dir_text, self.card.box, self.reason)
 
