321cac47c9744b5678b14cdedd60e85786183dc8
T S <yenthefirst@gmail.com>
2012-10-27 16:45:55 -0700

    set inventory status on import

--- scan_card.py
+++ scan_card.py
@@ -400,7 +400,7 @@ def folder_to_db(num):
 			path = os.path.join(dir, name)
 			img = open(path).read()
 
-			cursor.execute('insert into inv_cards (scan_png, box, box_index, recognition_status) values (?, ?, ?, ?)', [sqlite3.Binary(img), num, i, "scanned"])
+			cursor.execute('insert into inv_cards (scan_png, box, box_index, recognition_status, inventory_status) values (?, ?, ?, ?, ?)', [sqlite3.Binary(img), num, i, "scanned", "present"])
 		connection.commit()
 	finally:
 		connection.close()
